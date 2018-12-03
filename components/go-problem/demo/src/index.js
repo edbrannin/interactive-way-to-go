a9d82705972a835adaed9a02ac5c1cb04d8883f2
Ed Brannin <edbrannin@gmail.com>
2018-12-03 08:00:25 -0500

    Early progress on GoProblem

--- components/go-problem/demo/src/index.js
+++ components/go-problem/demo/src/index.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import GoProblem from '../../src'
+
+/*
+<data>
+  <name>50K-1</name>
+  <size>9</size>
+  <init>B[de]W[ee]B[ed]B[ef]</init>
+  <sequence>
+      <play pos="fe" message="success@red">
+      </play>
+      <play pos="zz" res="fe" message="failed@blue">
+      </play>
+  </sequence>
+</data>
+*/
 
 class Demo extends Component {
   render() {
     return <div>
-      <h1>go-problem Demo</h1>
-      <Example/>
+      <h1>9x Problem</h1>
+      <GoProblem
+        size={9}
+      />
+      <h1>Demo 19x</h1>
+      <GoProblem/>
     </div>
   }
 }
--- components/go-problem/src/index.js
+++ components/go-problem/src/index.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react'
+import { Goban } from 'react-goban'
 
-export default class extends Component {
+const aaToA1 = (coords) => {}
+
+const getStones = (stoneString) => {
+  return 0
+}
+
+class GoProblem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stones: {},
+      nextToPlay: this.props.nextToPlay || 'black'
+    }
+  }
+
+  onIntersectionClick = (event) => {
+    console.log('Clicked intersection', event);
+  }
+
   render() {
+    console.log('State:', this.state)
+    console.log('Goban:', Goban)
     return <div>
-      <h2>Welcome to React components</h2>
+      <Goban
+        size={this.props.size}
+        coordSystem="aa"
+        stones={this.state.stones || {}}
+        onIntersectionClick={this.onIntersectionClick}
+        nextToPlay={this.state.nextToPlay}
+      />
     </div>
   }
 }
+
+export default GoProblem
